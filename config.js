ceba2122b38eefc00bc5c6ecb457435cb00943a9
mlismailinh <86814545+mlismailinh@users.noreply.github.com>
2021-07-02 10:11:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Bae à!',
-    introDesc: 'Bếu',
+    introDesc: 'Anh gì ơi',
     btnIntro: 'Em ở đây',
-    title: 'Ăn đấm không?',
+    title: 'Ăn đấm bằng mồm không?',
     desc: 'Ăn đi',
-    btnYes: 'Có ăn <33',
-    btnNo: 'Không',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnYes: 'Cóaa',
+    btnNo: 'Không dou',
+    question:'Ai là Zu bếu nhỉ?',
+    btnReply: 'send to u',
+    reply: 'Người iu mkl :>',
+    mess: 'Em biết mà :))) Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Ôm cái nha.',
+    btnAccept: 'Oceeee luôn <3',
+    messLink: 'https://giphy.com/gifs/molangofficialpage-love-cute-9Y1LEFKsbbP4hrLgV3' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
